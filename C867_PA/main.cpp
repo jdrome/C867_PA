fae7c4d1421539ef754344ea57b943116aabd42a
jdrome <josedanilromero@gmail.com>
2025-04-27 19:34:59 -0500

    Initial Commit

---
+++ C867_PA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C867_PA
+//
+//  Created by Jose D. Romero on 4/27/25.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
