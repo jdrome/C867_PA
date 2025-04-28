62f640c5b03db3eb9690156fce149d8dc33f56a0
jdrome <josedanilromero@gmail.com>
2025-04-27 19:46:56 -0500

    Add own data to input data for student A5

--- C867_PA/main.cpp
+++ C867_PA/main.cpp
@@ -16,5 +16,5 @@ int main() {
                                     "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
                                     "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
                                     "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-                                    "A5,[firstname],[lastname],[emailaddress],[age], [numberofdaystocomplete3courses],SOFTWARE"};
+                                    "A5,Jose,Romero,jrom387@wgu.edu,26, 20,25,30,SOFTWARE"};
 }
