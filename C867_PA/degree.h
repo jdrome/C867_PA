0d239093d3d4a5be4d7ffb16145a7ab53af17e2d
jdrome <josedanilromero@gmail.com>
2025-04-27 19:58:18 -0500

    Task C:  Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.

--- C867_PA/degree.h
+++ C867_PA/degree.h
@@ -5,3 +5,10 @@
 //  Created by Jose D. Romero on 4/27/25.
 //
 
+#include <iostream>
+using namespace std;
+
+// Task C:  Define an enumerated data type DegreeProgram for the degree programs containing the data type values SECURITY, NETWORK, and SOFTWARE.
+
+// Define an enum for degree programs
+enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
